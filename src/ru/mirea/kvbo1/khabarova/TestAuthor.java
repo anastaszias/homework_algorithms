6cc9d0cf520268b9e7e9930ba0332acce9539822
Стася <103033198+anastaszias@users.noreply.github.com>
2022-09-18 05:31:30 +0300

    KVBO-01-21 Khabarova Practicе_1
    3 Практика по дисциплине Введение в алгоритмы и структуры данных систем реального времени

---
+++ src/ru/mirea/kvbo1/khabarova/TestAuthor.java
@@ -0,0 +1,7 @@
+package ru.mirea.kvbo1.khabarova;
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author a = new Author("Анастасия", "anastaszias@an.ru", 'w');
+        System.out.println(a);
+    }
+}
